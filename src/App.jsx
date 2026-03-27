7289e88b19b04b8fd3d7115345bef0822a08af05
S-KishoreBabu <s.kishorebabu8@gmail.com>
2026-03-27 12:44:25 +0530

    modified app.jsx & created home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 
 
 function App() {
 
   return (
-    <>
-    <Login/>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path= "/" element={<Home/>} />
+      <Route path= "/register" element={<Register/>} />
+    </Routes>
   )
 }
 
