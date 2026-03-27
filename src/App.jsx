ec27b1f13d27758e7d1913ebea316bb38de0f335
S-KishoreBabu <s.kishorebabu8@gmail.com>
2026-03-27 12:52:01 +0530

    enabled react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
-
+import Home from "./pages/Home"
+import Register from "./pages/Register"
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path= "/" element={<Home/>} />
-      <Route path= "/register" element={<Register/>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path= "/" element={<Home/>} />
+        <Route path= "/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
